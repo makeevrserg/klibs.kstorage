cdb36eba1d95f32211fd504a00554ba0c800dde0
Roman Makeev <makeevrserg@gmail.com>
2025-11-15 00:22:33 +0300

    add stateflow test

--- kstorage/src/commonTest/kotlin/ru/astrainteractive/klibs/kstorage/suspend/DefaultFlowMutableKrateTest.kt
+++ kstorage/src/commonTest/kotlin/ru/astrainteractive/klibs/kstorage/suspend/DefaultFlowMutableKrateTest.kt
@@ -1,5 +1,6 @@
 package ru.astrainteractive.klibs.kstorage.suspend
 
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.filterNotNull
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.firstOrNull
@@ -55,5 +56,20 @@ class DefaultFlowMutableKrateTest {
         assertNull(krate.flow.filterNotNull().firstOrNull())
         assertEquals(1, krate.getValue())
         assertEquals(1, krate.stateFlow(backgroundScope).first())
+    }
+
+    @Test
+    fun GIVEN_krate_with_replay_flow_WHEN_save_THEN_saved() = runTest {
+        val stateFlow = MutableStateFlow(1)
+        val krate = DefaultFlowMutableKrate(
+            factory = { 1 },
+            loader = { stateFlow },
+            saver = { stateFlow.emit(it) }
+        )
+        assertEquals(1, krate.getValue())
+        assertEquals(1, krate.flow.first())
+        krate.save(2)
+        assertEquals(2, krate.getValue())
+        assertEquals(2, krate.flow.first())
     }
 }
