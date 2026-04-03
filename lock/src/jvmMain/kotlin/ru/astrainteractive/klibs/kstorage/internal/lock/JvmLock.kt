4219a42e6ffdb1ade99b65cf56f96e94daca28ab
Roman Makeev <makeevrserg@gmail.com>
2026-04-04 01:40:01 +0300

    fix jvm lock

--- lock/src/jvmMain/kotlin/ru/astrainteractive/klibs/kstorage/internal/lock/JvmLock.kt
+++ lock/src/jvmMain/kotlin/ru/astrainteractive/klibs/kstorage/internal/lock/JvmLock.kt
@@ -1,12 +1,15 @@
 package ru.astrainteractive.klibs.kstorage.internal.lock
 
 import kotlinx.coroutines.runBlocking
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class JvmLock : Lock {
+    private val reentrantLock = ReentrantLock()
     override var isLocked: Boolean = false
 
     override fun <T> withLock(block: () -> T): T {
-        return synchronized(this) {
+        return reentrantLock.withLock {
             isLocked = true
             val value = block.invoke()
             isLocked = false
